7706142112f0ed903233704d42b38ac210e63e60
Christian Alexsander <christianalexbh@hotmail.com>
2020-06-10 16:23:26 -0300

    link modulo 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const IndexPage: React.FC = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }} />
     <Link to="/modulo-1-1/">Modulo 1 - 1 </Link>
     <Link to="/modulo-1-2/">Modulo 1 - 2 </Link>
-    <Link to="/modulo-1-2/">Modulo 1 - 3 </Link>
+    <Link to="/modulo-1-3/">Modulo 1 - 3 </Link>
   </>
 );
 
